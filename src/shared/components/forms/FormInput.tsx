f34614eeab0649503ef3a79da1dea74b2a4004d2
ibrahim <ibra.rodriguez.olaya@gmail.com>
2026-08-13 09:16:15 -0600

    fix: remove extra space in className of FormInput component

--- src/shared/components/forms/FormInput.tsx
+++ src/shared/components/forms/FormInput.tsx
@@ -9,7 +9,7 @@ export default function FormInput(props: Props) {
   return (
     <input
       {...props}
-      className={clsx('h-12 w-full rounded-md border border-slate-300 bg-white  pr-4 text-sm text-slate-900 outline-none transition placeholder:text-slate-400 focus:border-red-600 focus:ring-3 focus:ring-red-100', className)}
+      className={clsx('h-12 w-full rounded-md border border-slate-300 bg-white pr-4 text-sm text-slate-900 outline-none transition placeholder:text-slate-400 focus:border-red-600 focus:ring-3 focus:ring-red-100', className)}
     />
   )
 }
